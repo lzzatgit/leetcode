4fe302485d5956240a9fff17359cbca5e65cacf7
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-09-29 21:44:57 -0700

    Create sol.py

---
+++ Linked List/141. Linked List Cycle/sol.py
@@ -0,0 +1,20 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: ListNode) -> bool:
+        if not head:
+            return False
+        fast = head.next
+        slow = head
+
+        while fast and fast.next:
+            if fast == slow:
+                return True
+            fast = fast.next.next
+            slow = slow.next
+
+        return False
