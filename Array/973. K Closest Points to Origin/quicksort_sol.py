48e7f740408bffc00c1f80e4335a891b9196c3d9
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-10-17 10:51:13 -0700

    sols

---
+++ Array/973. K Closest Points to Origin/quicksort_sol.py
@@ -0,0 +1,29 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+
+        def dist(x):
+            return x[0]**2 + x[1]**2
+
+        def partition(i, j): # quick sort algorithm
+            pivot = points[j]
+            l = i - 1
+            for r in range(i, j):
+                rp = points[r]
+                if dist(rp) < dist(pivot):
+                    l += 1
+                    points[l], points[r] = points[r], points[l]
+            points[j], points[l+1] = points[l+1], points[j]
+            return l+1
+
+        def sort(i, j, k):
+            if i >= j: return
+
+            mid = partition(i, j)
+            if mid - i + 1 == k: return
+            elif mid - i + 1 < k:
+                sort(mid+1, j, k - (mid - i + 1) )
+            else:
+                sort(i, mid-1, k)
+
+        sort(0, len(points)-1, k)
+        return points[:k]
