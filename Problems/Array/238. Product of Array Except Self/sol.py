4db465fc68a9e50aec211fcb18ff16dc289e0e9d
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-10-28 18:19:08 -0700

    Create sol.py

---
+++ Problems/Array/238. Product of Array Except Self/sol.py
@@ -0,0 +1,15 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        answer = [0] * n
+        answer[0] = 1
+
+        for i in range(1, n):
+            answer[i] = answer[i-1] * nums[i-1]
+
+        R = 1
+        for i in reversed(range(n)):
+            answer[i] = answer[i] * R
+            R *= nums[i]
+
+        return answer
