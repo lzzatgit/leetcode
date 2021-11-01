90bcd484e9cceea73ed0b96d320fd2404828a3fd
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-11-01 10:43:14 -0700

    Create sol.py

---
+++ Learn/Binary Search/template_1/69. Sqrt(x)/sol.py
@@ -0,0 +1,15 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        l = 0
+        r = x
+
+        while l <= r:
+            mid = (l + r) // 2
+            if mid ** 2 == x:
+                return mid
+            elif mid ** 2 > x:
+                if (mid - 1) ** 2 < x:
+                    return mid - 1
+                r = mid - 1
+            else:
+                l = mid + 1
