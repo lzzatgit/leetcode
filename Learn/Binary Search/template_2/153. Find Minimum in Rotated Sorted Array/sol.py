e507ae64eeaf65a6ac6296ea0900a9f990f1590c
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-11-01 21:41:13 -0700

    Create sol.py

---
+++ Learn/Binary Search/template_2/153. Find Minimum in Rotated Sorted Array/sol.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        l = 0
+        r = len(nums) - 1
+
+        if nums[0] <= nums[r]:
+            return nums[0]
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if nums[mid] > nums[mid+1]:
+                return nums[mid+1]
+
+            if nums[mid-1] > nums[mid]:
+                return nums[mid]
+
+            if nums[mid] < nums[l]:
+                r = mid -1
+            else:
+                l = mid + 1
+                
