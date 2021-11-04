1fc28052b6820099dfd8d745d8e9c12c4ceea677
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-11-04 14:39:31 -0700

    Create sol.py

---
+++ Learn/Binary Search/367. Valid Perfect Square/sol.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        l = 0
+        r = num
+
+        while l <= r:
+            mid = (l + r) // 2
+
+            if mid ** 2 == num:
+                return True
+            elif mid ** 2 < num:
+                l = mid + 1
+            else:
+                r = mid - 1
+
+        return False
