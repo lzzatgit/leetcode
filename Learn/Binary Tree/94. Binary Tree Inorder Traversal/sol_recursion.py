87dc2e89fe4e08969e7b5aa0e0997715d357a9a2
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-11-08 19:54:18 -0800

    inorder tranversal bt

---
+++ Learn/Binary Tree/94. Binary Tree Inorder Traversal/sol_recursion.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+
+        def inorder(root):
+            if root == None:
+                return None
+            if root.left != None:
+                inorder(root.left)
+            res.append(root.val)
+            if root.right != None:
+                inorder(root.right)
+
+        inorder(root)
+        return res
