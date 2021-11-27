a05d82adc3e0673869d5a68290df2473c601f7e5
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-11-26 21:48:46 -0800

    Create sol.py

---
+++ Learn/Array 101/Intro/485. Max Consecutive Ones/sol.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findMaxConsecutiveOnes(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+
+        cnt = max_cnt = 0
+
+        for num in nums:
+            if num == 1:
+                cnt += 1
+            else:
+                max_cnt = max(max_cnt, cnt)
+                cnt = 0
+
+        return max(max_cnt, cnt)
+        
