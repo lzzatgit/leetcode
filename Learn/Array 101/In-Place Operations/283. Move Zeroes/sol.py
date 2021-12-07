7231915983a8d5680f4b89554723906ac8f57887
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-12-07 15:33:05 -0800

    Create sol.py

---
+++ Learn/Array 101/In-Place Operations/283. Move Zeroes/sol.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        pos = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                if pos != i:
+                    nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
+        return
+
+# leetcode solution comment
+# time O(N)
+# space O(1)
