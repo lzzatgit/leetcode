8912dac316779a78ff332aa07e581afbb08b0b78
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2021-12-11 11:29:43 -0800

    Update sol.py

--- Problems/Single Linked List/706. Design HashMap/sol.py
+++ Problems/Single Linked List/706. Design HashMap/sol.py
@@ -1,50 +1,37 @@
 class MyHashMap:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.key_space = 2046
-        self.hash_map = [[] for i in range(self.key_space)]
+        self.size = 769
+        self.hashmap = [[] for i in range(self.size)]
+
+    def _hash(self, key):
+        return key % self.size
 
 
     def put(self, key: int, value: int) -> None:
-        """
-        value will always be non-negative.
-        """
-        hash_key = key % self.key_space
+        pos = self._hash(key)
         found = False
-        for k, v in enumerate(self.hash_map[hash_key]):
-            if key == v[0]:
-                self.hash_map[hash_key][k] = (key,value)
+        for idx, ele in enumerate(self.hashmap[pos]):
+            if key == ele[0]:
+                self.hashmap[pos][idx] = (key, value)
                 found = True
+                break
         if not found:
-            self.hash_map[hash_key].append((key,value))
-
+            self.hashmap[pos].append((key,value))
 
     def get(self, key: int) -> int:
-        """
-        Returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key
-        """
-        hash_key = key % self.key_space
-        for (k,v) in self.hash_map[hash_key]:
-            if k == key:
+        pos = self._hash(key)
+        for (k, v) in self.hashmap[pos]:
+            if key == k:
                 return v
         return -1
 
 
     def remove(self, key: int) -> None:
-        """
-        Removes the mapping of the specified value key if this map contains a mapping for the key
-        """
-        hash_key = key % self.key_space
-        for k, v in enumerate(self.hash_map[hash_key]):
-            if key == v[0]:
-                del self.hash_map[hash_key][k]
+        pos = self._hash(key)
+        for idx, ele in enumerate(self.hashmap[pos]):
+            if key == ele[0]:
+                del self.hashmap[pos][idx]
 
-
-# Your MyHashMap object will be instantiated and called as such:
-# obj = MyHashMap()
-# obj.put(key,value)
-# param_2 = obj.get(key)
-# obj.remove(key)
+# time: O(K/N): N is the number of all possible keys and K is the number of predefined buckets
+# space: O(K + M): M is the number of unique keys that are inserted into the bucket
