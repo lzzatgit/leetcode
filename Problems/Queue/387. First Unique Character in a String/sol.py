1eaf082c1092a9fb5e70324853b267a9b2bb62ba
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2022-01-14 21:08:45 -0800

    Create sol.py

---
+++ Problems/Queue/387. First Unique Character in a String/sol.py
@@ -0,0 +1,9 @@
+from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        counts = Counter(s)
+
+        for idx, char in enumerate(s):
+            if counts[char] == 1:
+                return idx
+        return -1
