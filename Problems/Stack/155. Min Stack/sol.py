7fc6aed18a73d2e5437970a82c592e7654bfd573
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2022-01-18 20:23:42 -0800

    Create sol.py

---
+++ Problems/Stack/155. Min Stack/sol.py
@@ -0,0 +1,32 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if len(self.min_stack) == 0:
+            cur_min = val
+        else:
+            cur_min = min(val, self.min_stack[-1])
+        self.min_stack.append(cur_min)
+
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_stack[-1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
