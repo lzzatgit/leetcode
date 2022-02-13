8a5f40581d5a82ca956259ccbd06e4522d5278d1
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2022-02-13 11:46:42 -0800

    Create sol.py

---
+++ Problems/DFS/BinaryTree/1110. Delete Nodes And Return Forest/sol.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def delNodes(self, root: Optional[TreeNode], to_delete: List[int]) -> List[TreeNode]:
+        res = []
+        to_delete = set(to_delete)
+
+        def dfs(node):
+            if not node:
+                return None
+            node.left = dfs(node.left)
+            node.right = dfs(node.right)
+
+            if node.val in to_delete:
+                if node.left:
+                    res.append(node.left)
+                if node.right:
+                    res.append(node.right)
+                return None
+            return node
+
+        dfs(root)
+        if root.val not in to_delete:
+            res.append(root)
+        return res
