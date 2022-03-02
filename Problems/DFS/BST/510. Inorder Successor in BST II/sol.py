13fe54d0795042459981157dd12b004e34c8e91c
lzzatgit <91430216+lzzatgit@users.noreply.github.com>
2022-03-02 08:20:01 -0800

    Create sol.py

---
+++ Problems/DFS/BST/510. Inorder Successor in BST II/sol.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
+class Solution:
+    def inorderSuccessor(self, node: 'Node') -> 'Optional[Node]':
+        if not node:
+            return None
+
+        if node.right:
+            successor = node.right
+            while successor.left:
+                successor = successor.left
+            return successor
+
+        while node.parent and node == node.parent.right:
+            node = node.parent
+
+        return node.parent
+
+# leetcode Solution
+# time O(H) where H is the height of the tree
+# space O(1)
